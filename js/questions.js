613c2ee8338565376bfa613782136064929c4552
lisset2012 <lisset2012@gmail.com>
2018-06-03 15:21:10 -0400

    added functions

--- js/questions.js
+++ js/questions.js
@@ -1,3 +1,34 @@
+function getMusicQ(){
+    return [
+        `<div class = "musicq" id = "musicq1"><p><b>In what year did Pink FLoyd perform his first concert of The Wall</b></p></div>`,//1981
+        `<div class = "musicq" id = "musicq2"><p><b>What musician is often referred to as The Fifth Beatle</b></p></div>`,//Pete Best, was the drummer for The Beatles from 1960-1962; he was later replaced by Ringo Starr.
+        `<div class = "musicq" id = "musicq3"><p><b>Which one of Prince's songs reached highest on the music charts?</b></p></div>`//When Dove's Cry. Prince, full name Prince Rogers Nelson, was an American singer-songwriter.
+    ]
+}
+function getHistoryQ(){
+    return [
+        `<div class = "historyq" id = "historyq1"><p><b>Ulysses S. Grant appears on the front of which denomination of US. currency</b></p></div>`,//$50
+        `<div class = "historyq" id = "historyq2"><p><b>In the 1984 vice presidential debates, who was George H.W. Bush's opponent?</b></p></div>`,//Geraldine Ferraro
+        `<div class = "historyq" id = "historyq3"><p><b>On what day of the week did the Normandy Invasion of June 6, 1944 take place?</b></p></div>`//Tuesday
+    ]
+}
+function getGeographyQ(){
+    return [
+        `<div class = "geographyq" id = "geographyq1"><p><b>With over 35 million residents, what is the most populous city in the world?</b></p></div>`,//Tokyo
+        `<div class = "geographyq" id = "geographyq2"><p><b>How many U.S. states border the Gulf of Mexico?</b></p></div>`,//Five - From east to west:Florida, Alabama, Mississippi, Louisiana, and Texas.
+        `<div class = "geographyq" id = "geographyq3"><p><b>"Which country is home to the World Heritage Site Machu Picchu?</b></p></div>`//Peru
+    ]
+}
+function getSportQ(){
+    return [
+        `<div class = "sportq" id = "sportq1"><p><b>How many holes are there in a full round of golf?</b></p></div>`,//A traditional golf course is 18 holes, but nine-hole courses are common and can be played twice through for a full round of 18 holes.
+        `<div class = "sportq" id = "sportq2"><p><b>Who is the only athlete ever to play in a Super Bowl and a World Series?</b></p></div>`,//Deion Sanders is now a former American football and baseball player who works as an analyst for CBS Sports and the NFL Network.
+        `<div class = "sportq" id = "sportq3"><p><b>What city hosted the 2012 Summer Olympics?</b></p></div>`//London, England. London is the capital of England and is often known for the Queen's Guards, The London Eye, and Big Ben.
+    ]
+}
+
+
+
 var musicq = {
     musicq1: "In what year did Pink FLoyd perform his first concert of The Wall",//1981
     musicq2: "What musician is often referred to as The Fifth Beatle",//Pete Best, was the drummer for The Beatles from 1960-1962; he was later replaced by Ringo Starr.
--- js/script.js
+++ js/script.js
@@ -1,24 +1,73 @@
 //select elements and create variables
 var main = document.querySelector(".main")
 var music = document.querySelector(".music")//button music
-var history = document.querySelector(".history")//button history
+var hist = document.querySelector(".history")//button history
 var geography = document.querySelector(".geography")//button geography
 var sport = document.querySelector(".sport")//button sport
+
+let musicQSet = document.querySelector(".musicQ")
+
+// var musciq = randomMusicQ(getMusicQ())
+
+let musicQArray = getMusicQ()
+
+var player = {
+    score: 0
+}
 
 
 //define functions
 //defining functions for the first 4 buttons
+function randomMusicQ(musicQArray){
+    musicQSet.innerHTML = ""
+
+    var temp = []
+
+    for(var i = 0; i<musicQArray.length)
+}
 
 function musicQ() {
     main.innerHTML = `
 <div>
-<p><b>In what year did Pink FLoyd perform his first concert of The Wall</b></p>
+<p class = "musicQ"><b>${randomMusicQ()}</b></p>
 </div>
 
 <div>
-    <button class="answerr">1981</button>
-    <button class="answerw">1990</button>
+    <button class="musicAnswerRL1">1981</button>
+    <button class="musicAnswerWL1">1990</button>
 </div>`
+
+    var musicAnswerRL1 = document.querySelector(".musicAnswerRL1")
+    var musicAnswerWL1 = document.querySelector(".musicAnswerWL1")
+
+    musicAnswerRL1.addEventListener('click', function (event) {
+        player.score += 10;
+        main.innerHTML = `
+<div>
+<h3>Well Done! You just gained ${player.score} points. Keep going! Try the next Trivia</h3>
+<p><b>What musician is often referred to as The Fifth Beatle?</b></p>
+</div>
+
+<div>
+    <button class="musicAnswerRL2">Pete Best</button>
+    <button class="musicAnswerWL2">Michael Jackson</button>
+</div>`
+})
+
+
+    musicAnswerWL1.addEventListener('click', function (event) {
+        main.innerHTML = `
+<div>
+<h3>:( Wrong answer!</h3>
+     <p>Since you answered wrong you are redirected to another Trivia. Get the rigth answer to come back to your track</p>
+<p><b>Ulysses S. Grant appears on the front of which denomination of US. currency</b></p>
+</div>
+
+<div>
+    <button class="musicAnswerRL2">$50</button>
+    <button class="musicAnswerWL2">$100</button>
+</div>`
+    })
 }
 
 function historyQ() {
@@ -28,8 +77,8 @@ function historyQ() {
 </div>
 
 <div>
-    <button class="answerr">$50</button>
-    <button class="answerw">$100</button>
+    <button class="historyAnswerRL1">$50</button>
+    <button class="historyAnswerWL1">$100</button>
 </div>`
 }
 
@@ -40,8 +89,8 @@ function geographyQ() {
 </div>
 
 <div>
-    <button class="answerr">Tokyo</button>
-    <button class="answerw">Moscow</button>
+    <button class="geographyAnswerRL1">Tokyo</button>
+    <button class="geographyAnswerWL1">Moscow</button>
 </div>`
 }
 
@@ -52,26 +101,27 @@ function sportQ() {
 </div>
 
 <div>
-    <button class="answerr">18</button>
-    <button class="answerw">19</button>
+    <button class="sportAnswerRL1">18</button>
+    <button class="sportAnswerWL1">19</button>
 </div>`
 }
 
 //add event listeners and call the functions
 //adding listeners for the 4 first buttons
 
-music.addEventListener('click', function (event){
+music.addEventListener('click', function (event) {
+    console.log(event)
     musicQ();
 })
 
-history.addEventListener('click', function (event){
+hist.addEventListener('click', function (event) {
     historyQ();
 })
 
-geography.addEventListener('click', function (event){
+geography.addEventListener('click', function (event) {
     geographyQ();
 })
 
-sport.addEventListener('click', function (event){
+sport.addEventListener('click', function (event) {
     sportQ();
 })
